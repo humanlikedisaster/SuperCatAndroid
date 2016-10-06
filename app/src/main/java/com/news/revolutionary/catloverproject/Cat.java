44c43453dffc70c60c166dbd4656e83f76fb8a51
Alexandr Frantskevich <frantskevich.alexandr@gmail.com>
2016-10-07 00:32:48 +0300

    Fixes

--- app/src/main/java/com/news/revolutionary/catloverproject/Cat.java
+++ app/src/main/java/com/news/revolutionary/catloverproject/Cat.java
@@ -4,11 +4,8 @@ package com.news.revolutionary.catloverproject;
  * Created by hereiam on 25.09.16.
  */
 
-public class Cat {
-    public Man owner;
-    public int age;
-    public int loveLevel;
-
+public class Cat extends Pet {
+    @Override
     public void goAway()
     {
         //Если выгоняем кота - у него уменьшается уровень любви к хозяину
--- app/src/main/java/com/news/revolutionary/catloverproject/Dog.java
+++ app/src/main/java/com/news/revolutionary/catloverproject/Dog.java
@@ -4,13 +4,15 @@ package com.news.revolutionary.catloverproject;
  * Created by ACIDxBOYx161 on 05.10.2016.
  */
 
-public class Dog {
-    public int age;
-    public int loveLevel;
-    public Man owner;
+public class Dog extends Pet {
+    public int hungry;
+
+    @Override
     public void goAway()
     {
+        // Собака при любви меньше 50 должна убивать владельца.
         loveLevel -= 10;
+
         if (loveLevel < -50)
         {
             owner.kill();
